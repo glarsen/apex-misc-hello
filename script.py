84eeada38c94628448e8bd4219ca6e91748d0e99
Garrett Larsen <garrett.larsen@protonmail.com>
2018-03-21 03:40:27 -0500

    Add Sentry exception tracking

--- script.py
+++ script.py
@@ -3,11 +3,16 @@
 import os
 import sys
 
+import raven
+
+rclient = raven.Client()
+
 flag = None
 try:
     with open('/run/secrets/hello-flag') as f:
         flag = f.read()
 except FileNotFoundError:
+    rclient.captureException()
     flag = os.environ.get('FLAG')
 
 if not flag:
@@ -18,7 +23,14 @@ if not flag:
     sys.exit(1)
 
 print("Say hello")
-response = input()
+try:
+    response = input()
+except EOFError:
+    rclient.captureException()
+    print("[!] FATAL: Unexpected input. Aborting.",
+          file=sys.stderr)
+    print("Come at me brah!")
+    sys.exit(1)
 
 if response == "hello":
     print("Here's your flag: {0}".format(flag))
