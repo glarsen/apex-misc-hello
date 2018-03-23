9493b0d21c4fa7a727469199270d7d43e3409bef
Garrett Larsen <garrett.larsen@protonmail.com>
2018-03-23 01:23:34 -0500

    Refactor script to improve error reporting
    Also updates the script to use a standard name for the
    flag file.

--- script.py
+++ script.py
@@ -5,35 +5,57 @@ import sys
 
 import raven
 
-rclient = raven.Client()
+rclient = None
 
-flag = None
-try:
-    with open('/run/secrets/flag') as f:
-        flag = f.read()
-except FileNotFoundError:
-    rclient.captureException()
-    flag = os.environ.get('FLAG')
+def set_trace():
+    env = os.environ.get('APEX_ENV', "development")
+    global rclient
+    rclient = raven.Client(
+        site='hack.utulsa.cc',
+        environment=env
+    )
 
-if not flag:
-    print("[!] FATAL: No flag present.",
-          file=sys.stderr)
-    print("SERVICE ERROR: Challenge misconfigured -- "\
-          "Contact admin@utulsa.cc")
-    sys.exit(1)
+def load_flag():
+    flag = None
+    try:
+        with open('/run/secrets/flag') as f:
+            flag = f.read()
+    except FileNotFoundError:
+        flag = os.environ.get('FLAG')
 
-print("Say hello")
-try:
-    response = input()
-except EOFError:
-    rclient.captureException()
-    print("[!] FATAL: Unexpected input. Aborting.",
-          file=sys.stderr)
-    print("Come at me brah!")
-    sys.exit(1)
+    return flag
 
-if response == "hello":
-    print("Here's your flag: {0}".format(flag))
-else:
-    print("bruh..")
+def challenge():
+    print("Say hello")
+    try:
+        response = input()
+    except EOFError:
+        rclient.captureException(
+            message="Possible port scan attempt",
+            level='warning'
+        )
+        print("[!] FATAL: Unexpected input. Aborting.",
+              file=sys.stderr)
+        print("Come at me brah!")
+        sys.exit(1)
+
+    if response == "hello":
+        print("Here's your flag: {0}".format(flag))
+    else:
+        print("bruh..")
+
+if __name__ == "__main__":
+    set_trace()
+    flag = load_flag()
+    if not flag:
+        rclient.capture('raven.events.Message',
+            message="Initialization Error -- failed to load flag",
+            level='fatal'
+        )
+        print("[!] FATAL: No flag present.",
+              file=sys.stderr)
+        print("SERVICE ERROR: Challenge misconfigured -- "\
+              "Contact admin@utulsa.cc")
+        sys.exit(1)
+    challenge()
 
